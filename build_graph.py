9134ff2328de42aedac586bb6e62a759b0e4026a
Siddharth Narayanan <sid@futurehouse.org>
2024-04-27 18:33:30 -0700

    finding shortest path

--- build_graph.py
+++ build_graph.py
@@ -2,6 +2,7 @@
 
 import json
 import networkx as nx
+import pickle
 
 
 def read_json_file(file_path):
@@ -16,22 +17,34 @@ if __name__ == "__main__":
     data = read_json_file(file_path)
     graph = nx.Graph()
 
-    graph_dict = {}
     for json_object in data:
-        table_name = json_object["TableName"]
+        table_name = json_object["TableName"].upper()
         columns = json_object["Columns"]
         column_names = [column["ColumnName"] for column in columns]
         comments = [column["Comment"] for column in columns]
+
+        if table_name not in graph:
+            graph.add_node(table_name, node_type="table")
+
         table_dict = {}
-        for e, comment in enumerate(comments):
-            column_name = column_names[e]
+        for e, (column, comment) in enumerate(zip(column_names, comments)):
+            column = column.upper()
+            if column not in graph:
+                graph.add_node(column, node_type="column")
+
+            graph.add_edge(table_name, column)
+
             if "Foreign key to " in comment:
                 foreign_relationship = (
                     comment.split("Foreign key to ")[1].split("table")[0].lower()
                 )
                 if "the" in foreign_relationship:
                     foreign_relationship.split("the")[1].strip()
-            else:
-                foreign_relationship = None
-            table_dict[column_name.lower()] = foreign_relationship
-        graph_dict[table_name.lower()] = table_dict
+
+                foreign_relationship = foreign_relationship.strip().upper()
+                if foreign_relationship not in graph:
+                    graph.add_node(foreign_relationship, node_type="table")
+                graph.add_edge(column, foreign_relationship)
+
+    with open("schema_graph.pkl", "wb") as f:
+        pickle.dump(graph, f)
